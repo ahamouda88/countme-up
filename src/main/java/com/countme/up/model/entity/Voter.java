32fb703214f61ae2cab2d8a1117a0cceb697ba2d
ahamouda88 <ahmed.hamouda8888@gmail.com>
2017-05-10 14:52:50 -0400

    initial commit

---
+++ src/main/java/com/countme/up/model/entity/Voter.java
@@ -0,0 +1,14 @@
+package com.countme.up.model.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * An entity that represents the voter, with the list of votes
+ * 
+ * @author ahamouda
+ *
+ */
+@Entity
+public class Voter {
+
+}
